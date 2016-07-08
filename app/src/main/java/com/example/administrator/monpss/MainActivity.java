3590a55b96e67fe5b1ee1ef1ffb8584cec3106f8
Liamql <liamqly@gmail.com>
2016-07-08 17:15:06 +0800

    for merge test4

--- app/src/main/java/com/example/administrator/monpss/MainActivity.java
+++ app/src/main/java/com/example/administrator/monpss/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends Activity {
     /*git test*/
     /*reset*/
     /*branch*/
-    /*666*/
     ExpandableListView mainlistview = null;
     ArrayList<Entity> parent = null;
     ExpandAdapter mainAdapter;
